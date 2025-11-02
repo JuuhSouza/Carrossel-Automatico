334f731dccb95ff94103f735469ce4ddf6f0fc57
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-11-02 15:12:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const elemSlides = document.querySelector(".slides");
+const elemBotaoEsquerdo = document.querySelector(".esquerda");
+const elemBotaoDireito = document.querySelector(".direita");
+const elemImg = document.querySelectorAll(".slides img");
+
+let slide = 0;
+const tamanhoLista = elemImg.length -1;
+
+elemBotaoEsquerdo.addEventListener("click", () => /* () significa arrow function */{
+    slide--;
+    if(slide < 0) slide = elemImg.length - 1; /* voltar para o ultimo slide */
+    atualizarCarrossel();
+});
+
+elemBotaoDireito.addEventListener("click", () =>{
+    incrementarSlide();
+    atualizarCarrossel();
+});
+
+const incrementarSlide = () =>{
+     slide++; /* passar mais 1 */
+    if(slide > elemImg.length - 1) slide = 0; /*voltar para o primeiro slide  */
+}
+
+const atualizarCarrossel = () => {
+    elemSlides.style.transform = `translateX(-${slide * 100}%)`; /* fazer o calculo para passar para o lado pois para trocar de img é preciso calcular o tamnaho * 2 */
+};
+
+setInterval(() =>{
+    incrementarSlide();
+    atualizarCarrossel();
+}, 3000); /* temporizador automatico  */
